ccb9e1904fb1f67120cadbc4964224ab0c4f1978
A-Amyan <ayman.amyan@t-online.de>
2025-04-24 11:00:12 +0200

    add the primary class

--- bench/multi/PredictableCryptographicKeyABMCCase1.java
+++
@@ -1,9 +0,0 @@
-package org.cryptoapi.bench.predictablecryptographickey;
-
-public class PredictableCryptographicKeyABMCCase1 {
-    public static void main(String [] args){
-        PredictableCryptographicKeyABMC1 pc = new PredictableCryptographicKeyABMC1();
-        String key = "defaultkey";
-        pc.go(key);
-    }
-}
--- bench/multi/PredictablePBEPasswordABMCCase1.java
+++ bench/multi/PredictablePBEPasswordABMCCase1.java
@@ -1,5 +1,21 @@
 package org.cryptoapi.bench.predictablepbepassword;
 
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
+import java.security.SecureRandom;
+
+public class PredictablePBEPasswordABMC1 {
+    private PBEKeySpec pbeKeySpec = null;
+    private PBEParameterSpec pbeParamSpec = null;
+    public void key(String password) {
+        byte [] salt = new byte[16];
+        SecureRandom sr = new SecureRandom();
+        sr.nextBytes(salt);
+        int iterationCount = 11010;
+        int keyLength = 16;
+        pbeKeySpec = new PBEKeySpec(password.toCharArray(),salt,iterationCount,keyLength);
+    }
+}
 public class PredictablePBEPasswordABMCCase1 {
     public static void main(String [] args){
         PredictablePBEPasswordABMC1 ckp = new PredictablePBEPasswordABMC1();
